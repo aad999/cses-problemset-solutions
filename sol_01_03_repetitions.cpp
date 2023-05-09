3800d9d82f4d8781f1f2ed36fab506d53a123f66
addission <108756447+aad999@users.noreply.github.com>
2023-05-10 01:48:40 +0530

    01 03 solution updated

--- sol_01_03_repetitions.cpp
+++ sol_01_03_repetitions.cpp
@@ -7,15 +7,18 @@ int main()
     getline(cin, str);
     int count = 1;
     int ans = 1;
-    for(int i = 0; i < str.length()-1; i++){
-        if(str[i] == str[i+1]){
+    for (int i = 0; i < str.length() - 1; i++)
+    {
+        if (str[i] == str[i + 1])
+        {
             count++;
         }
-        else{
+        else
+        {
             count = 1;
         }
         ans = max(ans, count);
     }
-    cout<<ans<<'\n';
+    cout << ans << '\n';
     return 0;
 }
